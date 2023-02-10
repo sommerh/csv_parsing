02f298a8be296f5497a9c1949714dd11e83efe83
Hannah Sommer <hannah.j.sommer@gmail.com>
2023-02-10 16:29:05 -0500

    Create function that makes frequency tables from datasets
    Function can take as many parameters (i.e. filepaths) as needed

--- zipCodes.js
+++ zipCodes.js
@@ -5,40 +5,52 @@ const freqTable = {};
 const users = new Set();
 
 const parseSingleCsv = (filePath) => {
-  fs.createReadStream(filePath)
-    .pipe(parse({ delimiter: ',', from_line: 2 }))
-    .on('data', (row) => {
-      const userId = row[0];
-      const zip = row[row.length - 1];
+  return new Promise(function (resolve) {
+    fs.createReadStream(filePath)
+      .pipe(parse({ delimiter: ',', from_line: 2 }))
+      .on('data', (row) => {
+        const userId = row[0];
+        const zip = row[row.length - 1];
 
-      if (!users.has(userId)) {
-        if (!(zip in freqTable)) freqTable[zip] = 0;
-        freqTable[zip]++;
+        if (!users.has(userId)) {
+          if (!(zip in freqTable)) freqTable[zip] = 0;
+          freqTable[zip]++;
 
-        users.add(userId);
-      }
-    })
-    .on('error', (error) => {
-      console.log('😵‍💫', error.message);
-    })
-    .on('end', () => {
-      console.log('🥳 Parsing Complete');
-      console.log(freqTable);
-      console.log(
-        `We have ${users.size} users in ${
-          Object.keys(freqTable).length
-        } different zip codes!`
-      );
-    });
+          users.add(userId);
+        }
+      })
+      .on('error', (error) => {
+        console.log('😵‍💫', error.message);
+      })
+      .on('end', () => {
+        console.log('🥳 Parsing Complete');
+        resolve();
+      });
+  });
 };
 
-parseSingleCsv('./data/Group01.csv');
-parseSingleCsv('./data/Group02.csv');
-parseSingleCsv('./data/Group03.csv');
-parseSingleCsv('./data/Group04.csv');
-parseSingleCsv('./data/Group05.csv');
-parseSingleCsv('./data/Group06.csv');
-parseSingleCsv('./data/Group07.csv');
-parseSingleCsv('./data/Group08.csv');
-parseSingleCsv('./data/Group09.csv');
-parseSingleCsv('./data/Group10.csv');
+const addToTable = (...path) => {
+  const paths = [...path];
+
+  Promise.all(paths.map(parseSingleCsv)).then(() => {
+    console.log(freqTable);
+    console.log(
+      `We have ${users.size} users in ${
+        Object.keys(freqTable).length
+      } different zip codes!`
+    );
+  });
+};
+
+addToTable(
+  './data/Group01.csv',
+  './data/Group02.csv',
+  './data/Group03.csv',
+  './data/Group04.csv',
+  './data/Group05.csv',
+  './data/Group06.csv',
+  './data/Group07.csv',
+  './data/Group08.csv',
+  './data/Group09.csv',
+  './data/Group10.csv'
+);
