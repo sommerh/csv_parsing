1ebe0c4f65cb24094b453d495b3b3f7ba226eca5
Hannah Sommer <hannah.j.sommer@gmail.com>
2023-02-10 16:09:19 -0500

    Create one frequency table from all provided csv files
    This is able to create one large frequency table by calling the function 10 times. The next step will be to attempt to provide a more efficient way to add multiple files to the table

--- zipCodes.js
+++ zipCodes.js
@@ -4,28 +4,41 @@ const { parse } = require('csv-parse');
 const freqTable = {};
 const users = new Set();
 
-fs.createReadStream('./data/test.csv')
-  .pipe(parse({ delimiter: ',', from_line: 2 }))
-  .on('data', (row) => {
-    const userId = row[0];
-    const zip = row[row.length - 1];
+const parseSingleCsv = (filePath) => {
+  fs.createReadStream(filePath)
+    .pipe(parse({ delimiter: ',', from_line: 2 }))
+    .on('data', (row) => {
+      const userId = row[0];
+      const zip = row[row.length - 1];
 
-    if (!users.has(userId)) {
-      if (!(zip in freqTable)) freqTable[zip] = 0;
-      freqTable[zip]++;
+      if (!users.has(userId)) {
+        if (!(zip in freqTable)) freqTable[zip] = 0;
+        freqTable[zip]++;
 
-      users.add(userId);
-    }
-  })
-  .on('error', (error) => {
-    console.log('😵‍💫', error.message);
-  })
-  .on('end', () => {
-    console.log('🥳 Parsing Complete');
-    console.log(freqTable);
-    console.log(
-      `We have ${users.size} users in ${
-        Object.keys(freqTable).length
-      } different zip codes!`
-    );
-  });
+        users.add(userId);
+      }
+    })
+    .on('error', (error) => {
+      console.log('😵‍💫', error.message);
+    })
+    .on('end', () => {
+      console.log('🥳 Parsing Complete');
+      console.log(freqTable);
+      console.log(
+        `We have ${users.size} users in ${
+          Object.keys(freqTable).length
+        } different zip codes!`
+      );
+    });
+};
+
+parseSingleCsv('./data/Group01.csv');
+parseSingleCsv('./data/Group02.csv');
+parseSingleCsv('./data/Group03.csv');
+parseSingleCsv('./data/Group04.csv');
+parseSingleCsv('./data/Group05.csv');
+parseSingleCsv('./data/Group06.csv');
+parseSingleCsv('./data/Group07.csv');
+parseSingleCsv('./data/Group08.csv');
+parseSingleCsv('./data/Group09.csv');
+parseSingleCsv('./data/Group10.csv');
