b515bca9dc886044b5208847323e2915de89b143
Hannah Sommer <hannah.j.sommer@gmail.com>
2023-02-10 15:28:02 -0500

    Access data

--- zipCodes.js
+++ zipCodes.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+const fs = require('fs');
+const { parse } = require('csv-parse');
+
+fs.createReadStream('./data/Group01.csv')
+  .pipe(parse({ delimiter: ',', from_line: 2 }))
+  .on('data', (data) => {
+    console.log(data);
+  })
+  .on('error', (error) => {
+    console.log('😵‍💫', error.message);
+  })
+  .on('end', () => {
+    console.log('🥳 Parsing Complete');
+  });
